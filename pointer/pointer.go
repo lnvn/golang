96dd099574d104612f45817faba6a36e2b319073
Long Nguyen <faker2407@gmail.com>
2025-11-27 20:09:59 +0700

    Pointer - add comment

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -10,6 +10,8 @@ type Config struct {
 }
 
 func Pointer() {
+	// Create a new and zero-value Config struct
+	// & return memory address of Config struct
 	configPointer := &Config{}
 	fmt.Println("Config Timeout: ", configPointer.Timeout)
 	fmt.Println("Config Server: ", configPointer.Server)
@@ -19,6 +21,7 @@ func Pointer() {
 	fmt.Println("Final Server: ", configPointer.Server)
 }
 
+// we have parameter c of type *Config - apointer to Config
 func SetDefaults(c *Config) {
 	if c.Timeout == 0 {
 		c.Timeout = 30
