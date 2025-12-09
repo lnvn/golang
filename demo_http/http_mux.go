b9454bf91f9502501dd81f110952ebd74b2318f3
Long Nguyen <faker2407@gmail.com>
2025-12-09 15:45:55 +0700

    add 1 more handler

--- demo_http/http_mux.go
+++ demo_http/http_mux.go
@@ -12,9 +12,15 @@ func newPeopleHandler() http.Handler {
 	})
 }
 
+func newAnimalHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is animal handler")
+	})
+}
+
 func MyMux() {
 	mux := http.NewServeMux()
 	mux.Handle("/people", newPeopleHandler())
-
+	mux.Handle("/animal", newAnimalHandler())
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
